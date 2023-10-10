8ade852adf5747c4072cc3f908b9652943b5d8ef
Paweł Kurek <pawel.kurek975@gmail.com>
2023-10-10 21:42:51 +0200

    init project

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GradeManager
+{
+
+    public class GradeManager
+    {
+
+        public void AddGrade(string studentName, int grade)
+        {
+            // TODO: Uzupełnij implementację metody AddGrade, aby dodawała nową ocenę do listy ocen ucznia.
+        }
+
+        public void RemoveGrade(string studentName, int grade)
+        {
+            // TODO: Uzupełnij implementację metody RemoveGrade, aby usuwała ocenę z listy ocen ucznia, jeśli istnieje.
+        }
+
+        public double CalculateAverageGrade(string studentName)
+        {
+            // TODO: Uzupełnij implementację metody CalculateAverageGrade, aby obliczała średnią ocen ucznia, jeśli istnieją oceny.
+        }
+    }
+
+}
