224b9f3c8f1c5aee235fe28c245343921db06422
Paweł Kurek <pawel.kurek975@gmail.com>
2023-10-11 21:58:27 +0200

    GradeManager class init

---
+++ GradeManager.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeManager
+{
+    class GradeManager
+    {
+        // Init grades dictionary for all students
+        Dictionary<string, List<int>> grades = new Dictionary<string, List<int>>();
+
+        public void AddGrade(string studentName, int grade)
+        {
+            // TODO: Uzupełnij implementację metody AddGrade, aby dodawała nową ocenę do listy ocen ucznia.
+            if (!grades.ContainsKey(studentName))
+            {
+                grades[studentName] = new List<int>();
+            }
+
+            grades[studentName].Add(grade);
+            Console.WriteLine("Grade added");
+        }
+
+        public void RemoveGrade(string studentName, int grade)
+        {
+            // TODO: Uzupełnij implementację metody RemoveGrade, aby usuwała ocenę z listy ocen ucznia, jeśli istnieje.
+            if (grades.ContainsKey(studentName))
+            {
+                grades[studentName].Remove(grade);
+                Console.WriteLine("Grade removed");
+            }
+            else
+            {
+                Console.WriteLine("Student doesn't exist");
+            }
+        }
+
+        public double CalculateAverageGrade(string studentName)
+        {
+            // TODO: Uzupełnij implementację metody CalculateAverageGrade, aby obliczała średnią ocen ucznia, jeśli istnieją oceny.
+            if (grades.ContainsKey(studentName))
+            {
+                List<int> studentGrades = grades[studentName];
+                double gradesSum = 0;
+
+                foreach (int grade in studentGrades)
+                {
+                    gradesSum += grade;
+                }
+
+                return gradesSum / studentGrades.Count;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public void GetGrades(string studentName)
+        {
+            try
+            {
+                List<int> studentGrades = new List<int>();
+
+                Console.Write($"{studentName} grades are: ");
+                foreach (int grade in grades[studentName])
+                {
+                    Console.Write($"{grade},");
+                }
+
+                Console.WriteLine("\n");
+            }
+            catch
+            {
+                Console.WriteLine($"There is no student named {studentName}");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,25 +1,79 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace GradeManager
 {
-
-    public class GradeManager
+    public class Program
     {
-
-        public void AddGrade(string studentName, int grade)
+        
+        static void Main(string[] args)
         {
-            // TODO: Uzupełnij implementację metody AddGrade, aby dodawała nową ocenę do listy ocen ucznia.
+            bool isOperationValid = false;
+
+            do
+            {
+                Console.Write("*** Grade Manager ***");
+                Console.WriteLine("Type 'a' to add grade");
+                Console.WriteLine("Type 'r' to remove grade");
+                Console.WriteLine("Type 'g' to get student grades");
+                Console.WriteLine("Type 'av' to check average of grades");
+
+                GradeManager gradeManager = new GradeManager();
+
+                string operationSymbol = Console.ReadLine();
+
+                if (operationSymbol == "a")
+                {
+                    Console.Write("Student name: ");
+                    string studentName = Console.ReadLine();
+
+                    Console.Write("Grade (1-6): ");
+                    string grade = Console.ReadLine();
+
+                    gradeManager.AddGrade(studentName, int.Parse(grade));
+                    isOperationValid = true;
+
+                }
+                else if (operationSymbol == "r")
+                {
+                    Console.Write("Student name: ");
+                    string studentName = Console.ReadLine();
+
+                    Console.Write("Grade (1-6): ");
+                    string grade = Console.ReadLine();
+
+                    gradeManager.RemoveGrade(studentName, int.Parse(grade));
+                    isOperationValid = true;
+
+                }
+                else if (operationSymbol == "g")
+                {
+                    Console.Write("Student name: ");
+                    string studentName = Console.ReadLine();
+
+                    gradeManager.GetGrades(studentName);
+                    isOperationValid = true;
+
+                }
+                else if (operationSymbol == "av")
+                {
+                    Console.Write("Student name: ");
+                    string studentName = Console.ReadLine();
+
+                    gradeManager.CalculateAverageGrade(studentName);
+                    isOperationValid = true;
+
+                }
+                else
+                {
+                    Console.WriteLine("Wrong symbol, try again...\n");
+                }
+
+            } while (isOperationValid);
         }
 
-        public void RemoveGrade(string studentName, int grade)
-        {
-            // TODO: Uzupełnij implementację metody RemoveGrade, aby usuwała ocenę z listy ocen ucznia, jeśli istnieje.
-        }
-
-        public double CalculateAverageGrade(string studentName)
-        {
-            // TODO: Uzupełnij implementację metody CalculateAverageGrade, aby obliczała średnią ocen ucznia, jeśli istnieją oceny.
-        }
     }
+
 
 }
